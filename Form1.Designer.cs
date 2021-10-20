bbe0447b14ad216eb9720c4c44d7346a0a511d31
Volkan <Volkan@VLKN08PC01>
2021-10-20 16:50:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace ListViewLiveSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lv_data = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // tb_search
+            // 
+            this.tb_search.Location = new System.Drawing.Point(13, 25);
+            this.tb_search.Name = "tb_search";
+            this.tb_search.Size = new System.Drawing.Size(151, 20);
+            this.tb_search.TabIndex = 1;
+            this.tb_search.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search";
+            // 
+            // lv_data
+            // 
+            this.lv_data.HideSelection = false;
+            this.lv_data.Location = new System.Drawing.Point(13, 51);
+            this.lv_data.Name = "lv_data";
+            this.lv_data.Size = new System.Drawing.Size(405, 350);
+            this.lv_data.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lv_data.TabIndex = 3;
+            this.lv_data.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 426);
+            this.Controls.Add(this.lv_data);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_search);
+            this.Name = "Form1";
+            this.Text = "ListView Live Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tb_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lv_data;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ListViewLiveSearch
+{
+    public partial class Form1 : Form
+    {
+        private List<Spacecraft> _spacecrafts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupListView();
+        }
+
+        private void SetupListView()
+        {
+            lv_data.View = View.Details;
+            lv_data.GridLines = true;
+            lv_data.FullRowSelect = true;
+            lv_data.Columns.Add("Name", 150);
+            lv_data.Columns.Add("Destination", 250);
+
+            //LISTVIEW
+            _spacecrafts = new List<Spacecraft>
+            {
+                new Spacecraft { Name = "Casini", Destination = "Saturn" },
+                new Spacecraft { Name = "Enterprise", Destination = "Andromeda" },
+                new Spacecraft { Name = "Challenger", Destination = "Pluto" },
+                new Spacecraft { Name = "New Horizon", Destination = "Asteroid Belt" },
+                new Spacecraft { Name = "Opportunity", Destination = "Mars" },
+                new Spacecraft { Name = "Curiosity", Destination = "Mars" },
+                new Spacecraft { Name = "Juno", Destination = "Jupiter" },
+                new Spacecraft { Name = "Apollo 15", Destination = "Monn" },
+                new Spacecraft { Name = "Apollo 17", Destination = "Moon" },
+                new Spacecraft { Name = "Voyager A", Destination = "Alpha Centauri" },
+                new Spacecraft { Name = "Voyager B", Destination = "Proxima Centauri" },
+                new Spacecraft { Name = "Rosetter", Destination = "Venus" },
+                new Spacecraft { Name = "Spitzer", Destination = "Uranus" },
+                new Spacecraft { Name = "Chandra", Destination = "Mercury" },
+                new Spacecraft { Name = "Pioneer", Destination = "Mars" },
+                new Spacecraft { Name = "Atlantis", Destination = "Jupiter" }
+            };
+
+            //POPULATE SPACECRAFTS
+            foreach (var s in _spacecrafts) lv_data.Items.Add(new ListViewItem(new[] { s.Name, s.Destination }));
+        }
+
+        private void SearchData(string searchTerm)
+        {
+            lv_data.Items.Clear();
+
+            foreach (var s in _spacecrafts)
+                if (s.Name.ToLower().Contains(searchTerm.ToLower()) ||
+                    s.Destination.ToLower().Contains(searchTerm.ToLower()))
+                    lv_data.Items.Add(new ListViewItem(new[] { s.Name, s.Destination }));
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SearchData(tb_search.Text);
+        }
+    }
+
+    internal class Spacecraft
+    {
+        public string Name { get; set; }
+        public string Destination { get; set; }
+    }
+}
